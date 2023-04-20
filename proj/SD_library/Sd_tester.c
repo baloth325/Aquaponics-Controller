f9d66c47f9a275f9492c524341e9c9d0b31ab9ab
Ken Liao <sethggoolsby@gmail.com>
2023-04-19 22:33:57 -0700

    add SD fat32 system

--- proj/SD_library/Sd_tester.c
+++ proj/SD_library/Sd_tester.c
@@ -7,9 +7,25 @@
 
 int main(void)
 {
+	uint8_t res[5], buf[512], token;
 	SPI_init();
-	SD_init();
+	uint8_t code = SD_init();
 	
+
+	if (code != 0)
+	{
+		DDRC |= (1<< PC0); PORTC |= (1<< PC0);
+	}
+	else
+	{
+		DDRB |= (1 << PB7); PORTB |= (1<<PB7);
+	}
+	_delay_ms(250);
+	_delay_ms(250);
+	_delay_ms(250);
+	_delay_ms(250);
+
+
 
 
 
